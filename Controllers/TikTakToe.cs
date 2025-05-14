a688c8301ca10856e8c87d906369fb853836a57b
Matteo <msammut@brsanalytics.com>
2025-05-14 14:15:43 +0200

    Initial commit

---
+++ Controllers/TikTakToe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TikTakToeSERVER.Controllers
+{
+    [ApiController]
+    [Route("tiktaktoe")]
+    public class TikTakToe : ControllerBase
+    {
+        public static char[] Summaries = new[]
+        {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+
+        private readonly ILogger<TikTakToe> _logger;
+
+        public TikTakToe(ILogger<TikTakToe> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public char[] Post()
+        {
+            var rng = new Random();
+            /*return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+
+                Summaries
+            })
+            .ToArray();*/
+            return Summaries;
+        }
+    }
+}
